9934dc7ff92c0290fda587f5aa35e6bccfb6370f
Sebastian Bensusan <sbensu@gmail.com>
2015-02-20 20:34:11 +0100

    Renamed to publish in Clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject om-async-tut/lein-template "0.1.8"
+(defproject om-intermediate-template/lein-template "0.1.9"
   :description "Om async tutorial"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+        :url "http://github.com/bensu/om-intermediate-template"})
