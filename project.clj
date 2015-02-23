88c9da2b0ecce70a072377acac5511000f9ef114
Sebastian Bensusan <sbensu@gmail.com>
2015-02-23 22:01:48 +0100

    Bumped up version to deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-intermediate-template/lein-template "0.1.11"
+(defproject om-intermediate-template/lein-template "0.1.12"
   :description "Om async tutorial"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse Public License"
